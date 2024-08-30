5e17952a9414c0b9a0b3e2cadca3bbef11a43cf1
HerculesKoutalidis <130990231+HerculesKoutalidis@users.noreply.github.com>
2024-08-30 12:01:58 +0300

    Removed added comment line (added from github) from local dir

--- models/Scenario_S2.1.py
+++ models/Scenario_S2.1.py
@@ -13,7 +13,6 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 
 #%%
-#This is a line added from github
 def experiment_function(H2_selling_price_per_kg, simulation_horizon_number_of_years):
     #RES input data
     energy_generation_dir = '../models_inputs/RES generation data'
